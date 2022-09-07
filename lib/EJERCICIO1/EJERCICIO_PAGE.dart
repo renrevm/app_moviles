595e433c7416cd55200e71e3396bac6787a964d3
renrevm <109566216+renrevm@users.noreply.github.com>
2022-09-07 18:47:58 -0300

    Initial commit

---
+++ lib/EJERCICIO1/EJERCICIO_PAGE.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class EjercicioBoton extends StatefulWidget {
+  const EjercicioBoton({key});
+
+  @override
+  State<EjercicioBoton> createState() => _EjercicioBotonState();
+}
+
+class _EjercicioBotonState extends State<EjercicioBoton> {
+  int opc = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ejercicio Botón'),
+        backgroundColor: Colors.lightGreen.shade100,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: 200,
+            color: Colors.lightBlue.shade100,
+            child: IndexedStack(
+              alignment: Alignment.center,
+              index: opc,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellow,
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 50,
+            color: Colors.black,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      opc = 0;
+                    });
+                  },
+                  child: Text('Color Rojo'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      opc = 1;
+                    });
+                  },
+                  child: Text('Color Verde'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      opc = 2;
+                    });
+                  },
+                  child: Text('Color Amarillo'),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
